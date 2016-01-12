4dc0b045b666802a152315000cd1b8eaf02dc2f0
brandao-wesley <brandao.wesley.wb@gmail.com>
2016-01-12 12:02:49 -0200

    entidade produto

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    class Produto
+    {
+        public int ProdutiId { get; set; }
+        public int Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
+
+
