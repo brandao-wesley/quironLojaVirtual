844948308efd5af4c979ee0d3f1e8ba4cc456cb7
brandao-wesley <brandao.wesley.wb@gmail.com>
2016-01-12 18:01:04 -0200

    com view

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Quiron.LojaVirtual.Dominio.Entidades
 {
-    class Produto
+    public class Produto
     {
-        public int ProdutiId { get; set; }
-        public int Nome { get; set; }
-        public string Descricao { get; set; }
+        public int Produtoid { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
         public decimal Preco { get; set; }
-        public string Categoria { get; set; }
+        public String Categoria { get; set; }
+
     }
 }
-
-
